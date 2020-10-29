80395624653def0d6238cdd9ca6e2e8fcec204e6
tofanpetru <tofanpetru12@gmail.com>
2020-10-29 12:48:46 +0200

    Add files via upload

---
+++ Rest APIs test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Rest_APIs_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        //default
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        //Metoda post, si se va specifica ruta impreuna cu metoda api-ului
+        [HttpPost("post/")]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        //post 2, doar ca cu un parametru luat din ruta ([FromRoute])
+        [HttpPost("post/{id}")]
+        public ActionResult<ApiItem> Post([FromBody] ApiItem apiItem,[FromRoute] int id,[FromHeader] string header)
+        {
+            return Ok(apiItem);//metoda helper cu param apiItem, va returna 200
+        }
+        //Metoda , si se va specifica ruta 
+        
+        public class ApiItem
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
