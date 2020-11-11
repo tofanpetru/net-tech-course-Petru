6aa54d7186ae9292cd92c6ee75585be659d2c721
tofanpetru <tofanpetru12@gmail.com>
2020-11-12 00:56:57 +0200

    added transfer

---
+++ PaymentSystem/BlazorApp3/Shared/TransferDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp3.Shared
+{
+    public class TransferDto
+    {
+        public string SourceWalletId { get; set; }
+        public string Username { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
