576171741eefa37805750a19905be4c07ef4451f
tofanpetru <tofanpetru12@gmail.com>
2020-11-15 16:32:56 +0200

    transfer minor update

--- PaymentSystem/BlazorApp3/Shared/TransferDto.cs
+++ PaymentSystem/BlazorApp3/Shared/TransferDto.cs
@@ -6,10 +6,6 @@ namespace BlazorApp3.Shared
 {
     public class TransferDto
     {
-<<<<<<< HEAD
-=======
-        public string SourceWalletId { get; set; }
->>>>>>> main
         public string Username { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
